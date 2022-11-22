8c7cbd525be08613a916cbcfb5c9d6597c07b231
giancoppola <giancoppola@hotmail.co.uk>
2022-11-22 12:32:43 +0000

    css clean up

--- main.js
+++ main.js
@@ -1,29 +1,55 @@
-
 // slick slider for the tech page
 $(document).ready(function(){
-    $('.multiple-items').slick({
+    $('.tech-slider').slick({
         dots: true,
-        arrows: false,
+        appendDots: '#slick-dots',
+        arrows: true,
+        nextArrow: document.getElementById('next'),
+        prevArrow: document.getElementById('prev'),
         infinite: true,
-        speed: 600,
+        speed: 400,
         slidesToShow: 1,
         slidesToScroll: 1,
         mobileFirst: true,
+        rows: 0,
         responsive: [
             {
                 breakpoint: 1080,
                 settings: {
                     slidesToShow: 4,
-                    slidesToScroll: 4,
-                    arrows: true
+                    slidesToScroll: 1,
+                    rows: 0,
+                    dots: true,
+                    appendDots: '#slick-dots',
+                    arrows: true,
+                    nextArrow: document.getElementById('next'),
+                    prevArrow: document.getElementById('prev')
                 }
             },
             {
                 breakpoint: 800,
                 settings: {
                     slidesToShow: 3,
-                    slidesToScroll: 3,
-                    arrows: false
+                    slidesToScroll: 1,
+                    rows: 0,
+                    dots: true,
+                    appendDots: '#slick-dots',
+                    arrows: true,
+                    nextArrow: document.getElementById('next'),
+                    prevArrow: document.getElementById('prev')
+                }
+            },
+            {
+                breakpoint: 540,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    rows: 0,
+                    dots: true,
+                    appendDots: '#slick-dots',
+                    arrows: true,
+                    nextArrow: document.getElementById('next'),
+                    prevArrow: document.getElementById('prev')
                 }
             }
         ]
