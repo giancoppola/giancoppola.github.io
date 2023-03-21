c1e4a1fce44014a805d15de1ee6361f04847d380
giancoppola <giancoppola@hotmail.co.uk>
2023-03-21 15:17:30 +0000

    job areas overlay and events

--- js/hamburg.js
+++ js/hamburg.js
@@ -1,1 +1,21 @@
-// comment
+
+const customerServiceOverlay = document.getElementById('customer-service-overlay');
+const customerServiceOverlayClose = document.getElementById('customer-service-overlay-close');
+customerServiceOverlay.addEventListener('click', function() {
+    var node = customerServiceOverlay;
+    openOverlay(node);
+})
+customerServiceOverlayClose.addEventListener('click', function() {
+    var node = customerServiceOverlay;
+    closeOverlay(node);
+})
+
+function closeOverlay(node) {
+    console.log('here');
+    node.classList.add('hide');
+}
+
+function openOverlay(node) {
+    console.log('here');
+    node.classList.remove('hide');
+}
