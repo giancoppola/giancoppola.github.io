17e623d7c45857e90b1d73542db8ab6f4261a6e1
giancoppola <giancoppola@hotmail.co.uk>
2023-03-31 16:00:27 +0100

    cotravel

---
+++ js/cotravel.js
@@ -0,0 +1,6 @@
+let alertArr = document.getElementsByClassName('alert');
+for (el of alertArr){
+    el.addEventListener('click', function(){
+        window.alert('Developed from this design - https://dribbble.com/shots/20663985-CoTravel-travel-service-website');
+    })
+}
