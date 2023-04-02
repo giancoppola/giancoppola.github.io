4e091a4b909e9a992561dfef24d521203502026c
giancoppola <44214851+giancoppola@users.noreply.github.com>
2023-04-02 20:29:07 +0100

    dialog modal

--- js/cotravel.js
+++ js/cotravel.js
@@ -1,6 +1,7 @@
-let alertArr = document.getElementsByClassName('alert');
+const designDialog = document.querySelector('#overlay-design-dialog');
+const alertArr = document.getElementsByClassName('alert');
 for (el of alertArr){
     el.addEventListener('click', function(){
-        window.alert('Developed from this design - https://dribbble.com/shots/20663985-CoTravel-travel-service-website');
+        designDialog.showModal();
     })
 }
